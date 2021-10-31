d1a9a3df5c31a06416cccb992a2166735d0e60df
Micah Cowan <micah@addictivecode.org>
2021-10-31 03:30:38 -0700

    Reworked reconcile.sh framework.

--- home-setup/reconcile.sh
+++ home-setup/reconcile.sh
@@ -2,21 +2,16 @@
 
 set -e -u -C
 
-# paths of files we manage, together with paths where we might
-# expect to find them for comparison purposes. Each record is
-# separated by :, paths in a record separated by spaces (with the first
-# field being "our" path
-paths=
-
 main() {
-    convert_profile_d
+    srcdir=$(dirname "$0")
 
-    paths_for_dotfiles
-    paths_for_available
-    paths_for_ext
-
-    handle_missing_files
-    reconcile_changes
+    if test "${1-__nothing__}" != --list; then
+        convert_profile_d
+    else
+        paths_for_dotfiles
+        paths_for_available
+        paths_for_ext
+    fi
 }
 
 convert_profile_d() {
@@ -53,152 +48,25 @@ convert_profile_d() {
 }
 
 paths_for_dotfiles() {
-    for file in $(cd dot-files && find . -type f); do
+    for file in $(cd "$srcdir"/dot-files && find . -type f); do
         file=${file#./}
-        paths_add paths dot-files/"$file" ~/"$file"
+        printf '%s %s\n' "$srcdir"/dot-files/"$file" ~/"$file"
     done
 }
 
-paths_add() {
-    varname=$1; shift
-    eval "${varname}=\"\${${varname}}\${${varname}:+:}\$*\""
-}
-
 paths_for_available() {
-    for file in rc-scripts/*; do
+    for file in "$srcdir"/rc-scripts/*; do
         name=${file##*/}
         dest="$HOME/.rc-available/${name}"
-        paths_add paths "$file" "$dest"
+        printf '%s %s\n' "$file" "$dest"
     done
 }
 
 paths_for_ext() {
-    paths_add paths ext/opt-pather/opt-path-setup.sh ~/.rc-available/opt-path-setup.sh
-    paths_add paths ext/promptjobs/prompt-jobs.sh ~/.rc-available/prompt-jobs.sh
-    paths_add paths ext/profile-loader/dot.runrc.sh ~/.bashrc
-    paths_add paths ext/profile-loader/dot.runrc.sh ~/.kshrc
-}
-
-handle_missing_files() {
-    feeder=${paths}
-    record=''
-
-    # First, identify the missing files
-    missing=
-    not_missing=
-    while shift_one_path_record feeder record; do
-        eval "set -- $record"
-        if test -e "$2"; then
-            # exists
-            paths_add not_missing "$@"
-        else
-            if test -z "$missing"; then
-                printf '%s\n' \
-                    "The following files are not present on your system:"
-            fi
-            paths_add missing "$@"
-            printf '  %s\n' "${2}"
-        fi
-    done
-
-    if test -n "$missing"; then
-        missing_copy=$missing
-        printf '%s' "Install (Yes/No/Itemize)? "
-        read answer
-        case "$answer" in
-            [Yy]*|[Ii]*)
-                while shift_one_path_record missing_copy record; do
-                    eval "set -- $record"
-                    do_it=
-                    case "$answer" in
-                        [Ii]*)
-                            printf 'copy %s (y/n)? ' "$2"
-                            read ans2
-                            case "$ans2" in
-                                [Yy]*)
-                                    do_it=yes
-                                    ;;
-                            esac
-                            ;;
-                        *)
-                            do_it=yes
-                            ;;
-                    esac
-                    if test -n "$do_it"; then
-                        mkdir -p "${2%/*}"
-                        cp -v "$1" "$2"
-                        if test "x$2" != "x${2#$HOME/.rc-available/}"; then
-                            # enable new rc files by default
-                            name=${2#$HOME/.rc-available/}
-                            ln -s ../.rc-available/"$name" ~/.rc-enabled/"$name"
-                        fi
-                    fi
-                done
-                ;;
-            *)
-                printf '%s\n\n' 'Doing nothing with missing files.'
-                ;;
-        esac
-    fi
-}
-
-reconcile_changes() {
-    # Depends on "missing" and "not_missing" list variable, set up in
-    # handle_missing_files()
-
-    # First, find differing files
-    differing=''
-    not_differing=''
-    while shift_one_path_record not_missing record; do
-        eval "set -- $record"
-        if cmp -s "$1" "$2" >/dev/null 2>/dev/null; then
-            paths_add not_differing "$@"
-        else
-            paths_add differing "$@"
-        fi
-    done
-
-    if test -n "$not_differing"; then
-        printf '%s\n' "The following files are unchanged on the system:"
-        while shift_one_path_record not_differing record; do
-            eval "set -- $record"
-            printf '  %s\n' "$2"
-        done
-    fi
-
-    if test -n "$differing"; then
-        differing_copy=$differing
-        printf '%s\n' "The following files are CHANGED on the system:"
-        while shift_one_path_record differing_copy record; do
-            eval "set -- $record"
-            printf '  %s\n' "$2"
-        done
-        printf '%s\n' "Press ENTER to reconcile"
-        read answer
-
-        # Reconcile!
-        while shift_one_path_record differing record; do
-            eval "set -- $record"
-            printf '+vimdiff %s %s\n' "$1" "$2"
-            vimdiff "$1" "$2"
-        done
-    fi
-}
-
-shift_one_path_record() {
-    varname=$1; shift
-    onename=$1; shift
-    if eval test -z "\"\$$varname\""; then
-        return 1
-    fi
-
-    eval "remain=\${${varname}#*:}"
-    eval "${onename}=\${${varname}%:\"\$remain\"}"
-    if eval test 'x"$remain"' = "x\"\$${varname}\""; then
-        remain=''
-    fi
-    eval "${varname}=\$remain"
-    return 0
+    printf '%s %s\n' "$srcdir"/ext/opt-pather/opt-path-setup.sh ~/.rc-available/opt-path-setup.sh
+    printf '%s %s\n' "$srcdir"/ext/promptjobs/prompt-jobs.sh ~/.rc-available/prompt-jobs.sh
+    printf '%s %s\n' "$srcdir"/ext/profile-loader/dot.runrc.sh ~/.bashrc
+    printf '%s %s\n' "$srcdir"/ext/profile-loader/dot.runrc.sh ~/.kshrc
 }
 
 main "$@"
