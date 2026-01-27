2a8b92efe4f26c39a0de4838e93e28eb2449de78
Micah Cowan <micah@addictivecode.org>
2026-01-26 22:27:24 -0800

    Install nvim config from github repo

--- vim-stuff/install.sh
+++ vim-stuff/install.sh
@@ -14,4 +14,16 @@ if test ! -d ~/.vim/bundle/vim-asm_ca65; then
     cp -a -v "$srcdir"/ext/vim-asm_ca65 ~/.vim/bundle/
 fi
 
+if test ! -e ~/.config/nvim/init.lua; then
+    mkdir -p ~/.config/nvim
+    (
+        cd ~/.config/nvim
+        git init -b main
+        git remote add origin \
+            ssh://git@github.com/micahcowan/nvim-config.git
+        git pull origin main
+        git branch -u origin/main main
+    )
+fi
+
 exit 0
